674c3cdf4f1d27c4e900d742c778335e39773029
jenyeu <jenyeuw@gmail.com>
2017-10-08 09:02:50 -0700

    Set up accessibility to cross.html

--- views.py
+++ views.py
@@ -38,14 +38,15 @@ def input_strains():
 def cross():
     strain_1 = request.form['strain-1']
     strain_2 = request.form['strain-2']
-    strn1 = Strain(strain_1, DETERMINANTS)
-    strn2 = Strain(strain_2, DETERMINANTS)
-    patho1 = strn1.pathogenicity
-    patho2 = strn2.pathogenicity
-    cross = CrossData(strn1, strn2, DETERMINANTS)
-    data = cross.data
-    return render_template('cross.html', result1=strain_1, result2=strain_2,
+#    strn1 = Strain(strain_1, DETERMINANTS)
+#    strn2 = Strain(strain_2, DETERMINANTS)
+#    patho1 = strn1.pathogenicity
+#    patho2 = strn2.pathogenicity
+#    cross = CrossData(strn1, strn2, DETERMINANTS)
+#    data = cross.data
+#    return render_template('cross.html', result1=strain_1, result2=strain_2,
                             patho1=patho1, patho2=patho2, data=data)
+    return render_template('cross.html', result1=strain_1, result2=strain_2)
 
 
 if __name__ == "__main__":
